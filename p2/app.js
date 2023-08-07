6c3304ef0c6638e05672c622bf45de6957c0293a
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-07 14:24:04 +0530

    hide, show person list, local storage save, load

--- p2/app.js
+++ p2/app.js
@@ -1,12 +1,40 @@
-const { useState } = React;
+const { useState, useEffect } = React;
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [tasks, setTasks] = useState([]);
+  const [assignedTasks, setAssignedTasks] = useState({});
 
   const [newPerson, setNewPerson] = useState('');
   const [newTask, setNewTask] = useState('');
   const [assignedPerson, setAssignedPerson] = useState('');
+
+  // State to track whether the person list is visible or hidden
+  const [showPersonList, setShowPersonList] = useState(true);
+
+  // Load data from local storage on initial render
+  useEffect(() => {
+    const savedPersons = JSON.parse(localStorage.getItem('persons') || '[]');
+    const savedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    const savedAssignedTasks = JSON.parse(localStorage.getItem('assignedTasks') || '{}');
+
+    setPersons(savedPersons);
+    setTasks(savedTasks);
+    setAssignedTasks(savedAssignedTasks);
+  }, []);
+
+  // Save data to local storage whenever there's a change
+  useEffect(() => {
+    localStorage.setItem('persons', JSON.stringify(persons));
+  }, [persons]);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    localStorage.setItem('assignedTasks', JSON.stringify(assignedTasks));
+  }, [assignedTasks]);
 
   const handleAddPerson = () => {
     setPersons([...persons, newPerson]);
@@ -19,6 +47,25 @@ const App = () => {
     setAssignedPerson('');
   };
 
+  // Handle "Enter" key press in the person input field
+  const handlePersonKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      handleAddPerson();
+    }
+  };
+
+  // Handle "Enter" key press in the task input field
+  const handleTaskKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      handleAddTask();
+    }
+  };
+
+  // Handle toggle for showing/hiding the person list
+  const handleTogglePersonList = () => {
+    setShowPersonList(!showPersonList);
+  };
+
   return (
     <div className="container mx-auto mt-5">
       <div className="mb-5">
@@ -29,6 +76,7 @@ const App = () => {
           placeholder="Enter person name"
           value={newPerson}
           onChange={(e) => setNewPerson(e.target.value)}
+          onKeyDown={handlePersonKeyDown}
         />
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded ml-2"
@@ -38,13 +86,29 @@ const App = () => {
         </button>
       </div>
 
+      {/* Toggle button to show/hide the person list */}
       <div className="mb-5">
-        <h2 className="text-2xl font-bold">List of Persons:</h2>
-        <ul className="list-disc list-inside">
-          {persons.map((person, index) => (
-            <li key={index}>{person}</li>
-          ))}
-        </ul>
+        <h2 className="text-2xl font-bold">
+          <button
+            className="text-blue-500"
+            onClick={handleTogglePersonList}
+          >
+            {showPersonList ? 'Hide Person List' : 'Show Person List'}
+          </button>
+        </h2>
+        {showPersonList && (
+          <div>
+            {persons.length > 0 ? (
+              <ul className="list-disc list-inside">
+                {persons.map((person, index) => (
+                  <li key={index}>{person}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>No persons found.</p>
+            )}
+          </div>
+        )}
       </div>
 
       <div className="mb-5">
@@ -55,6 +119,7 @@ const App = () => {
           placeholder="Enter task description"
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
+          onKeyDown={handleTaskKeyDown}
         />
         <select
           className="border border-gray-400 px-2 py-1 rounded ml-2"
@@ -63,7 +128,9 @@ const App = () => {
         >
           <option value="">Assign to:</option>
           {persons.map((person, index) => (
-            <option key={index} value={person}>{person}</option>
+            <option key={index} value={person}>
+              {person}
+            </option>
           ))}
         </select>
         <button
